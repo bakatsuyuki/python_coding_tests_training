cd2ac6a1da75cab4138604e2497fb1bf23d3a2c0
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-04 21:25:32 +0900

    solve 001.py

--- typical90_atcoder/001.py
+++ typical90_atcoder/001.py
@@ -1,6 +1,9 @@
+import math
+
 n, l = map(int, input().split())
 k = int(input())
 a = list(map(int, input().split()))
+
 pieces = []
 for i in range(n):
     if i == 0:
@@ -9,21 +12,26 @@ for i in range(n):
         pieces.append(a[i] - a[i - 1])
 pieces.append(l - a[n - 1])
 
-for _ in range(n - k):
-    min_piece_index = pieces.index(min(pieces))
+min_score = min(pieces)
+max_score = math.floor(l / (k + 1))
 
-
-    def combine_pieces(target_index):
-        pieces[min_piece_index] += pieces[target_index]
-        del pieces[target_index]
-
-
-    if min_piece_index == 0:
-        combine_pieces(min_piece_index + 1)
-    elif min_piece_index == len(pieces) - 1:
-        combine_pieces(min_piece_index - 1)
-    elif pieces[min_piece_index - 1] > pieces[min_piece_index + 1]:
-        combine_pieces(min_piece_index + 1)
+while max_score != min_score:
+    i = math.floor((max_score + min_score) / 2)
+    if min_score == i:
+        i += 1
+    tmp = i
+    cut_count = -1
+    for piece in pieces:
+        if tmp >= i:
+            tmp = piece
+            cut_count += 1
+        else:
+            tmp += piece
+    last_peace = tmp
+    if cut_count > k:
+        min_score = i
+    if (tmp < i and cut_count <= k) or cut_count < k:
+        max_score = i - 1
     else:
-        combine_pieces(min_piece_index - 1)
-print(min(pieces))
+        min_score = i
+print(min_score)
