5403740ca6b7c1fcacce6a4858448a5077e9d3fb
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-06 19:11:48 +0900

    tmp 008

--- typical90_atcoder/008.py
+++ typical90_atcoder/008.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 import numpy as np
 
 n = int(input())
@@ -9,47 +7,40 @@ mod = 10 ** 9 + 7
 
 target_chars = 'atcoder'
 
+s = [x for i, x in enumerate(s) if x in target_chars]
+
 indexes = [[i for i, x in enumerate(s) if x == target_chars[index]] for index in range(len(target_chars))]
 
-
-def remove_errors_from_indexes(indexes):
-    has_changed = False
-    copied_indexes = deepcopy(indexes)
-    for i in range(len(target_chars)):
-        for j in range(len(indexes[i])):
-            index = indexes[i][j]
-            if i < len(indexes) - 1 and index > max(indexes[i + 1]):
-                has_changed = True
-                print('削除1')
-                print(f'i: {i}')
-                print(index)
-                print(indexes[i + 1])
-                del (copied_indexes[i][j])
-            elif i > 0 and index < min(indexes[i - 1]):
-                has_changed = True
-                print('削除2')
-                print(f'i: {i}')
-                print(index)
-                print(indexes[i - 1])
-                # コピーしてるからindexずれてる
-                # それのせいでバグってる
-                del (copied_indexes[i][j])
-    if has_changed:
-        return remove_errors_from_indexes(copied_indexes)
-    else:
-        return copied_indexes
-
-
-indexes = remove_errors_from_indexes(indexes)
-
 dp = np.array([[0] * n] * len(target_chars))
+
+
+def error_check():
+    if not all(indexes):
+        print('0')
+        exit()
+
+print(indexes)
+
+for i in range(len(indexes) - 1):
+    next_i = i + 1
+    indexes[next_i] = [index for _, index in enumerate(indexes[next_i]) if index > min(indexes[i])]
+    error_check()
+
+    reverse_i = len(indexes) - 1 - i
+    before_reverse_i = reverse_i - 1
+    indexes[before_reverse_i] = [index for _, index in enumerate(indexes[before_reverse_i]) if
+                                 index < max(indexes[reverse_i])]
+    error_check()
+
+print(indexes)
 
 for i in range(len(indexes)):
     for j in indexes[i]:
         if i == 0:
             dp[i][j:] += 1
-        elif dp[i - 1][j] != 0:
+        else:
             dp[i][j:] = (dp[i][j:] + dp[i - 1][j:]) % mod
 
-print(dp)
+print(''.join(s))
+print(len([x for i, x in enumerate(s) if x == 'a']))
 print(dp[len(target_chars) - 1][n - 1])
