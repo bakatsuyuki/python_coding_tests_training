fbab9ddeca67b51bb824be00cbad34997fc022e6
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-07 15:16:11 +0900

    solve 012

--- typical90_atcoder/012.py
+++ typical90_atcoder/012.py
@@ -1,8 +1,49 @@
+# sys.stdin = open('typical90_l_in_subtask_1_35.txt')
+
 h, w = map(int, input().split())
 q_count = int(input())
 q = [list(map(int, input().split())) for _ in range(q_count)]
 
 red_points_map = {}
+list_ref = []
+
+root_point = [-1, -1]
+
+
+class UnionFind:
+    def __init__(self):
+        self.parents = [[root_point for _ in range(w + 2)] for _ in range(h + 2)]
+
+    def find(self, point):
+        x, y = point
+        parent_point = self.parents[x][y]
+        if parent_point == root_point:
+            return root_point
+        elif parent_point == point:
+            return point
+        else:
+            node = self.find(parent_point)
+            return node
+
+    def same(self, point_a, point_b):
+        root_a = self.find(point_a)
+        root_b = self.find(point_b)
+        if root_a == root_point or root_b == root_point:
+            return False
+        return self.find(point_a) == self.find(point_b)
+
+    def union(self, point_a, point_b):
+        if point_a == root_point or point_b == root_point:
+            return
+        bx, by = self.find(point_b)
+        self.parents[bx][by] = self.find(point_a)
+
+    def add(self, point):
+        x, y = point
+        self.parents[x][y] = point
+
+
+uf = UnionFind()
 
 
 def point_to_key(r, c):
@@ -15,22 +56,19 @@ def key_to_point(key):
 
 def query_1(arguments):
     r, c = arguments
-    keys = [point_to_key(r + 1, c), point_to_key(r, c + 1), point_to_key(r - 1, c), point_to_key(r, c - 1)]
 
-    new_list = [arguments]
-    red_points_map[point_to_key(r, c)] = new_list
-    for key in keys:
-        if key in red_points_map:
-            new_list.extend(red_points_map[key])
-            for point in red_points_map[key]:
-                red_points_map[point_to_key(point[0], point[1])] = new_list
+    points = [[r + 1, c], [r, c + 1], [r - 1, c], [r, c - 1]]
+
+    uf.add(arguments)
+    for point in points:
+        point_node = uf.find(point)
+        if point_node != root_point:
+            uf.union(point_node, [r, c])
 
 
 def query_2(arguments):
     ra, ca, rb, cb = arguments
-    key_a = point_to_key(ra, ca)
-    key_b = point_to_key(rb, cb)
-    if key_a in red_points_map and key_b in red_points_map and red_points_map[key_a] == red_points_map[key_b]:
+    if uf.same([ra, ca], [rb, cb]):
         print('Yes')
     else:
         print('No')
