cb8d0c78f6bf4750aa243ee1fbf7b2261bbf1fbb
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-07 19:55:40 +0900

    tmp 013

--- typical90_atcoder/013.py
+++ typical90_atcoder/013.py
@@ -1,10 +1,9 @@
 import _pickle
-import sys
-from array import array
+import heapq
 
-from line_profiler_pycharm import profile
+# from line_profiler_pycharm import profile
 
-sys.stdin = open('typical90_m_in_killer_2.txt')
+# sys.stdin = open('typical90_m_in_killer_2.txt')
 n, m = map(int, input().split())
 abc = [list(map(int, input().split())) for _ in range(m)]
 
@@ -21,37 +20,21 @@ for a, b, c in abc:
     raw_load_map[b][a] = c
 
 
-@profile
+# @profile
 def dijkstra(start_at):
     load_map = _pickle.loads(_pickle.dumps(raw_load_map, -1))
-    unsettled_costs = {}
-    settled_indexes = array('i', [])
-    costs = {start_at: 0}
+    settled_indexes = []
+    costs = {}
     settled_indexes.append(start_at)
-    index = start_at
-    loop_count = 0
-
-    loop_count2 = 0
-    outer_loop_count = 0
-    for _ in range(n - 1):
-        destinations = []
-        outer_loop_count += 1
+    unsettled_costs = []
+    heapq.heappush(unsettled_costs, [0, start_at])
+    while unsettled_costs:
+        cost, index = heapq.heappop(unsettled_costs)
+        if index in costs:
+            continue
+        costs[index] = cost
         for destination in load_map[index]:
-            loop_count += 1
-            if destination not in unsettled_costs:
-                unsettled_costs[destination] = inf
-            cost = load_map[index][destination]
-            unsettled_costs[destination] = min(unsettled_costs[destination], costs[index] + cost)
-            destinations.append(destination)
-        del load_map[index]
-        for destination in destinations:
-            loop_count2 += 1
-            del load_map[destination][index]
-        min_index = min(unsettled_costs, key=unsettled_costs.get)
-        settled_indexes.append(min_index)
-        index = min_index
-        costs[index] = unsettled_costs[min_index]
-        del unsettled_costs[min_index]
+            heapq.heappush(unsettled_costs, [cost + load_map[index][destination], destination])
     return costs
 
 
