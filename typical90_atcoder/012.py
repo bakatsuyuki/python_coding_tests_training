eebed7daf8a821f5fcfa5bc37c6f84b82ad3d6b5
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-07 13:33:28 +0900

    tmp 012

--- typical90_atcoder/012.py
+++ typical90_atcoder/012.py
@@ -2,17 +2,35 @@ h, w = map(int, input().split())
 q_count = int(input())
 q = [list(map(int, input().split())) for _ in range(q_count)]
 
-board = [[False] * (h + 1) for _ in range(w + 1)]
+red_points_map = {}
+
+
+def point_to_key(r, c):
+    return f'{r} {c}'
+
+
+def key_to_point(key):
+    return map(int, key.split())
 
 
 def query_1(arguments):
     r, c = arguments
-    board[r][c] = True
+    keys = [point_to_key(r + 1, c), point_to_key(r, c + 1), point_to_key(r - 1, c), point_to_key(r, c - 1)]
+
+    new_list = [arguments]
+    red_points_map[point_to_key(r, c)] = new_list
+    for key in keys:
+        if key in red_points_map:
+            new_list.extend(red_points_map[key])
+            for point in red_points_map[key]:
+                red_points_map[point_to_key(point[0], point[1])] = new_list
 
 
 def query_2(arguments):
     ra, ca, rb, cb = arguments
-    if board[ra][ca] and board[rb][cb]:
+    key_a = point_to_key(ra, ca)
+    key_b = point_to_key(rb, cb)
+    if key_a in red_points_map and key_b in red_points_map and red_points_map[key_a] == red_points_map[key_b]:
         print('Yes')
     else:
         print('No')
