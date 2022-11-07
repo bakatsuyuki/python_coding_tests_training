44f8e9b99d12be6734e73bfa535318b12def7ed6
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-07 12:19:44 +0900

    solve 011

--- typical90_atcoder/011.py
+++ typical90_atcoder/011.py
@@ -4,22 +4,17 @@ dcs.sort()
 
 max_d = max([d for d, _, _ in dcs])
 
-candidate_ds = {}
+dp = [[0 for j in range(max_d + 1)] for i in range(n)]
 
-for d, c, s in dcs:
-    print('開始')
-    print(d)
-    if d in candidate_ds:
-        candidate_ds[d] = max([s, candidate_ds[d]])
-    else:
-        candidate_ds[d] = s
-    for cd in candidate_ds:
-        print(cd)
-        print(candidate_ds)
-        cs = candidate_ds[cd]
-        if cd + c <= d:
-            candidate_ds[d] = max([candidate_ds[d], s + cs])
+for i in range(len(dp)):
+    d, c, s = dcs[i]
+    for j in range(1, len(dp[i])):
+        if d < j:
+            break
+        current_s = s if c <= j else 0
+        additional_s = dp[i - 1][j - c] if i - 1 >= 0 and j - c >= 0 else 0
+        compare_s1 = dp[i - 1][j] if i - 1 >= 0 else 0
+        compare_s2 = dp[i][j - 1] if j - 1 >= 0 else 0
+        dp[i][j] = max([current_s + additional_s, compare_s1, compare_s2])
 
-print(candidate_ds)
-
-# print(candidate_ds[max_d])
+print(int(dp[-1][-1]))
