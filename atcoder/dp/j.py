93bf10b520ce4490eb440c5bf9476abda790ed8b
bakatsuyuki <bakatsuyuki@icloud.com>
2022-11-18 23:35:00 +0900

    tmp j

--- atcoder/dp/j.py
+++ atcoder/dp/j.py
@@ -1,8 +1,34 @@
+from line_profiler_pycharm import profile
+
 n = int(input())
-a = [int(input()) for _ in range(n)]
+a = list(map(int, input().split()))
 sushi_count = sum(a)
 
-dp = [[0] * (sushi_count + 1) for _ in range(n + 1)]
+dp = [[[None for _ in range(n + 1)] for _ in range(n + 1)] for _ in range(n + 1)]
 
-dp[0] = a
-print(dp)
+default_c1 = len([x for _, x in enumerate(a) if x == 1])
+default_c2 = len([x for _, x in enumerate(a) if x == 2])
+default_c3 = len([x for _, x in enumerate(a) if x == 3])
+dp[0][0][0] = 0
+
+
+@profile
+def solve(c1, c2, c3):
+    if dp[c1][c2][c3] is not None:
+        return dp[c1][c2][c3]
+
+    c0 = n - c1 - c2 - c3
+    p0 = c0 / n
+    denominator = 1
+    if c1 > 0:
+        denominator += solve(c1 - 1, c2, c3) * c1 / n
+    if c2 > 0:
+        denominator += solve(c1 + 1, c2 - 1, c3) * c2 / n
+    if c3 > 0:
+        denominator += solve(c1, c2 + 1, c3 - 1) * c3 / n
+    result = denominator / (1 - p0)
+    dp[c1][c2][c3] = result
+    return result
+
+
+print(solve(default_c1, default_c2, default_c3))
